69d9257cdbe98175fc5d2556ae05619b80279802
Jessica Wailes <jess.m.wailes@gmail.com>
2022-08-22 17:30:42 -0700

    added live indicator, fixed fam cards

--- js/countdown.js
+++ js/countdown.js
@@ -1,6 +1,6 @@
 var clientID = '0ystkdnbr06044vt9vahzod73l5wrm';
 var clientSecret = '2xsoytpdc66lbfzfcxi6xv8kaswbk5';
-var token, expiresIn, id, sked
+var token, expiresIn, id, streamingNow
 
 //TOKEN
 var getOauth = function () {
@@ -45,18 +45,30 @@ var getStreamSked = function(){
     }).then(function (res) {
         return res.json()
     }).then(function(data) {
-        sked = data.data.segments[0].start_time
-        console.log(sked)
+        //console.log(data.data.segments[0])
+        sked = data.data.segments
+        //console.log("start time", sked)
     }) 
 };
 
-var countdown = function (nextStream) {
+
+//refactor countdown to handle array of objects
+var countdown = function (upcomingStreams) { 
+    console.log("strims", upcomingStreams)
     var timer, days, hours, minutes, seconds;
-  
-    nextStream = new Date(nextStream);
     
+    console.log("starting out", upcomingStreams[0].start_time)
+    var nextStream = upcomingStreams[0].start_time
+    nextStream = new Date(nextStream); //this is converting to PDT, oops
+    nextStream = new Date(nextStream.getUTCFullYear(), //PUT IT BACK
+                               nextStream.getUTCMonth(),
+                               nextStream.getUTCDate(),
+                               nextStream.getUTCHours(),
+                               nextStream.getUTCMinutes(),
+                               nextStream.getUTCSeconds());
+    
+
     nextStream = nextStream.getTime();
-    //console.log("wait", nextStream)
 
     if ( isNaN(nextStream) ) {
       return;
@@ -72,14 +84,13 @@ var countdown = function (nextStream) {
                                dateStart.getUTCHours(),
                                dateStart.getUTCMinutes(),
                                dateStart.getUTCSeconds());
-        // console.log(dateStart);
-        //console.log(nextStream)
+
       var timeRemaining = parseInt((nextStream - dateStart.getTime()) / 1000)
-        // console.log(timeRemaining)
+
       if ( timeRemaining >= 0 ) {
         days    = parseInt(timeRemaining / 86400);
         timeRemaining   = (timeRemaining % 86400);
-        hours   = parseInt(timeRemaining / 3600) + 7; //fix this for seth's timezone
+        hours   = parseInt(timeRemaining / 3600); 
         if (hours >= 24) {
             days += 1;
             hours -= 24;
@@ -89,8 +100,92 @@ var countdown = function (nextStream) {
         timeRemaining   = (timeRemaining % 60);
         seconds = parseInt(timeRemaining);
   
+        document.getElementById("days").innerHTML    = parseInt(days, 10) + "D:";
+        document.getElementById("hours").innerHTML   = ("0" + hours).slice(-2) + "H:";
+        document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2) + "M:";
+        document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2) + "S";
+      } else {
+        return;
+      }
+    }
+  
+  }
+
+  var getStreamLive = function(){
+    return fetch('https://api.twitch.tv/helix/streams?user_login=sethdrums', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Client-Id': clientID
+        }
+    }).then(function (res) {
+        return res.json()
+    }).then(function(data) {
+        console.log("live streams", data.data)
+        streamingNow = data.data
+        //
+    }) 
+};
+
+var countup = function (streamingNow) {
+    //console.log(streamingNow[0].game_name)
+    var hours, minutes, seconds;
+    
+    //find the time:
+    var start_time = streamingNow[0].started_at;
+    console.log(start_time);
+    
+    start_time = new Date(start_time); //this is converting to PDT, oops
+    console.log(start_time)
+    start_time = new Date(start_time.getUTCFullYear(), //PUT IT BACK
+                            start_time.getUTCMonth(),
+                            start_time.getUTCDate(),
+                            start_time.getUTCHours(),
+                            start_time.getUTCMinutes(),
+                            start_time.getUTCSeconds());
+
+    start_time = start_time.getTime();
+
+    console.log(start_time);
+    if ( isNaN(start_time) ) {
+      return;
+    }
+  
+    timer = setInterval(calculate, 1000);
+  
+    function calculate() {
+      var dateStart = new Date();
+      var dateStart = new Date(dateStart.getUTCFullYear(),
+                               dateStart.getUTCMonth(),
+                               dateStart.getUTCDate(),
+                               dateStart.getUTCHours(),
+                               dateStart.getUTCMinutes(),
+                               dateStart.getUTCSeconds());
+
+      
+      
+      //logic: I need to take the current time and subtract start time
+      var timeRemaining = parseInt((dateStart.getTime() - start_time) / 1000)
+      //console.log("time rem", timeRemaining)
+
+      //need a "before now check"
+      if ( timeRemaining >= 0 ) {
+        days    = parseInt(timeRemaining / 86400);
+        timeRemaining   = (timeRemaining % 86400);
+        hours   = parseInt(timeRemaining / 3600); 
+        if (hours >= 24) {
+            days += 1;
+            hours -= 24;
+        } 
+        timeRemaining   = (timeRemaining % 3600);
+        minutes = parseInt(timeRemaining / 60);
+        timeRemaining   = (timeRemaining % 60);
+        seconds = parseInt(timeRemaining);
+        
+        //console.log("this is the hours", hours)
+
         document.getElementById("days").innerHTML    = parseInt(days, 10);
-        document.getElementById("hours").innerHTML   = ("0" + hours).slice(-2);
+        document.getElementById("hours").innerHTML   = parseInt(hours);//("0" + hours).slice(-2);
         document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
         document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
       } else {
@@ -98,7 +193,6 @@ var countdown = function (nextStream) {
       }
     }
   
-    //function display(days, hours, minutes, seconds) {}
   }
 
 
@@ -108,7 +202,39 @@ var makeCall = function () {
         getOauth().then(function () {
             getbroadcastID().then(function () {
                 getStreamSked().then(function () {
-                    countdown(sked)
+                    getStreamLive().then(function () {
+                        var livehide = document.getElementById("livehide");
+                        var countdownColor = document.getElementById("hideylink");
+                        var noworlater = document.getElementById("noworlater");
+                        var recButton = document.getElementById("pulse");
+                        var twitchbtn = document.getElementById("twitchbtn");
+                        if (streamingNow.length > 0) {
+                            countup(streamingNow);
+                            console.log("theres's a stream now")
+                            //change color of coundown to red, and set to 0's
+                            //countdown is whole counter. needs to be entirely red
+                            //noworlater is word. 'NOW' for this secttion                     
+                            livehide.style.display = "none";
+                            recButton.style.display = "block";
+                            //livehide.style = "color: rgba(255, 0, 0, 0) !important"
+                            countdownColor.style = "color: rgb(255, 0, 0) !important"
+                            noworlater.innerHTML = "STREAMING NOW!"
+                            twitchbtn.innerHTML = "LIVE NOW ON TWITCH"
+                        }
+                        else {
+                            console.log("NO stream! Coundown!")
+                            //countdown is whole counter. needs to be entirely white
+                            //noworlater is word. 'LATER' for this secttion
+                            livehide.style.display = "block";
+                            recButton.style.display = "none";
+                            countdownColor.style = "color: rgb(255, 255, 255) !important"
+                            noworlater.innerHTML = "NEXT STREAM: "
+                            noworlater.style = "margin-right: 10px;"
+                            //coundown to next stream
+                            countdown(sked)
+                        }
+                    });
+                    
                 });
             });
             return;
@@ -116,7 +242,15 @@ var makeCall = function () {
     } else{
         getbroadcastID().then(function() {
             getStreamSked().then(function () {
-                countdown(sked)
+                getStreamLive().then(function () {
+                    if (streamingNow .length > 0) {
+                        console.log("theres's a stream now")
+                    }
+                    else {
+                        console.log("NO stream! Coundown!")
+                        countdown(sked)
+                    }
+                });
             });
         });
     }; 
@@ -125,7 +259,4 @@ var makeCall = function () {
 
 
 makeCall();
-countdown();
-
-
-
+//countdown();
--- js/script.js
+++ js/script.js
@@ -9,4 +9,17 @@ function changeNav(){
 
 }
 
+function navbarAdjustments() {
+    var navbar = document.getElementById('scrolly');
+    var scrollVal = window.scrollY;
+    if (scrollVal < 151) {
+        console.log("ok, blue")
+        navbar.classList.contains('scrolled') ? navbar.classList.remove('scrolled') : navbar.classList.add('scrolled')
+    }else {
+        console.log("no color change")
+    }
+    
+}
+
+
 window.addEventListener('scroll', changeNav);
